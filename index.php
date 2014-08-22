720dac162b038c739800c7657f346f98a623c02d
HammerEngineer <mahfujhimel.bd@gmail.com>
2014-08-22 01:07:28 -0400

    sec

---
+++ index.php
@@ -0,0 +1,11 @@
+
+<?php 
+
+require_once('inc/autoload.php');
+require_once('controller/Core.php');
+//require_once('classes/Core.php');
+
+$core = new Core();
+$core->run();
+//echo Url::getParam();
+//echo $core->name;
